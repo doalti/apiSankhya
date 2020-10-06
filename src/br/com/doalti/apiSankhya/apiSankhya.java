733baab2796fe0115cd1f4f0d7db7d72794638e6
doalti <douglas.almeida@doalti.com.br>
2020-10-06 07:43:36 -0300

    Projeto Criado

---
+++ src/br/com/doalti/apiSankhya/apiSankhya.java
@@ -0,0 +1,11 @@
+package br.com.doalti.apiSankhya;
+
+public class apiSankhya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste github!");
+
+	}
+
+}
